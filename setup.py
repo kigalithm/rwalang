539ca2bc884916d170e3ab8e580aa41531976ecf
izamha <glcode40@gmail.com>
2025-05-08 15:16:44 +0200

    setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="kgt_rwalang",
-    version="1.0.1",
+    version="1.0.2",
     author="Kigalithm Ltd.",
     author_email="foss@kigalithm.com",
     description="An enhanced language detector for Kinyarwanda",
